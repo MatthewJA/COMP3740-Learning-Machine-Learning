9c01953937cae6466477cfe3f35c1592b0515447
Buck Shlegeris <bshlegeris@gmail.com>
2014-09-08 17:00:44 +1000

    more bits of rmi-mlp

--- rl/hlp_rmi.py
+++ rl/hlp_rmi.py
@@ -31,11 +31,54 @@ class RMI_Perceptron(hidden_layer_perceptron.Hidden_Layer_Perceptron):
 		"""
 		Get the symbolic cost.
 		"""
+		something = -theano.tensor.mean(
+			theano.tensor.log(self.get_probability_matrix())[
+				theano.tensor.arange(self.symbolic_output.shape[0]),
+				self.symbolic_output])
 
-		return (self.modulation * self.get_supervised_cost() +
-			(1-self.modulation) * self.get_unsupervised_cost() +
+
+
+		return
 			self.regularisation_weights["L1"] * self.regularisation["L1"] +
 			self.regularisation_weights["L2"] * self.regularisation["L2"])
+
+	# I probably need to change this?
+	def initialise_theano_functions(self):
+		"""
+		Compile Theano functions.
+		"""
+
+		index = theano.tensor.lscalar("i")
+		batch_size = theano.tensor.lscalar("s")
+
+		self.train_model_once = theano.function(inputs=[index, batch_size]
+			, outputs=self.get_cost()
+			, updates=self.calculate_updates()
+			, givens={
+				self.symbolic_input:
+					self.input_batch[index*batch_size:(index+1)*batch_size],
+				self.symbolic_output:
+					self.output_batch[index*batch_size:(index+1)*batch_size]})
+
+		self.validate_model = theano.function(inputs=[index, batch_size]
+			, outputs=self.error_rate(self.symbolic_output)
+			, givens={
+				self.symbolic_input:
+					self.validation_input_batch[
+						index*batch_size:(index+1)*batch_size],
+				self.symbolic_output:
+					self.validation_output_batch[
+						index*batch_size:(index+1)*batch_size]})
+
+	def train_model(self, epochs=100, minibatch_size=600, yield_every_iteration=False):
+		"""
+		Train the model against the given data.
+		"""
+
+		a bunch of times:
+			cost = self.train_model_once(thing, label)
+
+
 
 if __name__ == '__main__':
 	import lib.mnist as mnist
