29488295c0a21c657d4331385d9cff29b64e14ee
MatthewJA <matthew.alger@gmail.com>
2014-09-30 09:47:22 +1000

    Added depickled cart

--- rl/try_pickled_cart_pole_da.py
+++ rl/try_pickled_cart_pole_da.py
@@ -1,9 +1,10 @@
+from denoising_autoencoder_cart_pole import Cart_Pole_DA
 import cart_pole
 import cPickle
 
 with open("cartDA.pickle") as f:
 	agent = cPickle.load(f)
-	cart = cart_pole.Cart(0.1)
+	cart = cart_pole.Cart(1)
 
 	def get_action(cart):
 		return cart_pole.get_action(agent, cart)
